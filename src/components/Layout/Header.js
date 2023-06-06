596eb7c4d82bcbd40f5c85f7591a41b046ab8410
Precious <Preshomondi@gmail.com>
2023-06-06 13:36:30 +0300

    Create and style header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,19 +1,20 @@
-import React from 'react'
-import mealsImage from '../../assets/food.webp'
-import classes from './Header.module.css'
+import React from "react";
+import mealsImage from "../../assets/food.webp";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
-const Header = (props) =>  {
+const Header = (props) => {
   return (
     <React.Fragment>
-        <header className={classes.header}>
-            <h1>ReactMeals</h1>
-            <button>Cart</button>
-        </header>
-        <div className={classes['main-image']}>
-            <img src={mealsImage} alt="Table full of food"/>
-        </div>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Table full of food" />
+      </div>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
