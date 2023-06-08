6cc16f65122bb4124c69e2066c69bc2363694454
Precious <Preshomondi@gmail.com>
2023-06-08 10:23:45 +0300

    Use refs and forward refs

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useContext } from 'react'
+import CartContext from '../../store/cart-context'
 import MealForm from './MealForm'
 import classes from  './MealItem.module.css'
 
 function MealItem(props) {
   // const price = `$${props.price.toFixed(2)}`;
+  const cartCtx = useContext(CartContext)
+
+  const addToCartHandler = amount => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price
+    })
+  }
   return (
     <li className={classes.meal}>
         <div>
@@ -12,7 +24,7 @@ function MealItem(props) {
         <div className={classes.price}>${props.price}</div>
         </div>
         <div>
-          <MealForm/>  
+          <MealForm onAddToCart={addToCartHandler}/>  
         </div>
     </li>
   )
